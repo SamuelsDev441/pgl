5e0099ed2353251c63ff3c194a73d822ac4148bd
SamuelsDev441 <154631883+SamuelsDev441@users.noreply.github.com>
2024-02-29 18:14:25 -0500

    Updated net.py

--- .github/net.py
+++ .github/net.py
@@ -1,7 +1,7 @@
 import socket
 
 
-class Server:
+class TCPServer:
     def __init__(self):
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -14,3 +14,13 @@ class Server:
     def stick(self, server, addr):
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind(addr)
+
+
+class TCPClient:
+    def __init__(self):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+class UDPClient:
+    def __init__(self):
+        client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
